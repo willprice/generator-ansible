3d03ce4514629e97ab9744151fd48b543e051fd2
Will Price <will.t.price@boeing.com>
2016-02-23 13:34:08 +0000

    style(ansible, ansible_test): Bring code into standard eslint compliance

--- test/ansible_spec.js
+++ test/ansible_spec.js
@@ -1,50 +1,54 @@
-var path = require('path');
-var helpers = require('yeoman-test');
-var assert = require('yeoman-assert');
-var util = require('util');
+'use strict'
+/* eslint-env mocha */
+var path = require('path')
+var helpers = require('yeoman-test')
+var assert = require('yeoman-assert')
+var util = require('util')
 
-describe("yo ansible", function() {
-    var playbookName = "testPlaybookName";
-    var groups = ["testGroupOne", "testGroupTwo"];
-    var hosts = ["testHostOne.local", "testHostTwo.local"];
-    var plays = ["testPlayOne", "testPlayTwo"];
+describe('yo ansible', function () {
+  var playbookName = 'testPlaybookName'
+  var groups = ['testGroupOne', 'testGroupTwo']
+  var hosts = ['testHostOne.local', 'testHostTwo.local']
+  var plays = ['testPlayOne', 'testPlayTwo']
 
-    before(function(done) {
-        helpers.run(path.join(__dirname, '../app'))
-            .withPrompts({
-                playbookName: playbookName,
-                groups: groups,
-                hosts: hosts,
-                plays: plays,
-            })
-            .withGenerators([[helpers.createDummyGenerator(), 'license']])
-            .on('end', done);
-    });
+  before(function (done) {
+    helpers.run(path.join(__dirname, '../app'))
+      .withPrompts({
+        playbookName: playbookName,
+        groups: groups,
+        hosts: hosts,
+        plays: plays
+      })
+      .withGenerators([[helpers.createDummyGenerator(), 'license']])
+      .on('end', done)
+  })
 
-    it('produces a site.yml file', function() {
-        assert.file('site.yml');
-    });
+  it('creates a site.yml file', function () {
+    assert.file('site.yml')
+  })
 
-    it('README.md has name of playbook in it', function() {
-        assert.fileContent('README.md', playbookName);
-    });
+  describe('README.md', function () {
+    it('is created', function () {
+      assert.file('README.md')
+    })
 
-    it('produces a play file for each play specified', function() {
-        var playFiles = plays.map(function(play) { return util.format("plays/%s.yml", play); });
-        assert.file(playFiles);
-    });
+    it('has name of playbook in it', function () {
+      assert.fileContent('README.md', playbookName)
+    })
+  })
 
-    it('produces a host var file for each host', function() {
-        var hostVarFiles = hosts.map(function(host) { return util.format("host_vars/%s", host); });
-        assert.file(hostVarFiles);
-    });
+  it('creates a play file for each play specified', function () {
+    var playFiles = plays.map(function (play) { return util.format('plays/%s.yml', play) })
+    assert.file(playFiles)
+  })
 
-    it('produces a play file for each play', function() {
-        var playFiles = plays.map(function(play) { return util.format("plays/%s.yml", play); });
-        assert.file(playFiles);
-    });
+  it('creates a playbook for each play', function () {
+    var playFiles = plays.map(function (play) { return util.format('plays/%s.yml', play); })
+    assert.file(playFiles)
+  })
 
-    it('produces a README.md file', function() {
-        assert.file('README.md');
-    });
-});
+  it('creates a host var file for each host', function () {
+    var hostVarFiles = hosts.map(function (host) { return util.format('host_vars/%s', host); })
+    assert.file(hostVarFiles)
+  })
+})
